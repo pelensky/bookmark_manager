55b7f0cb053af790eb2c8674fe6fb92f3f7385a5
Dan Pelensky <dan@pelensky.com>
2016-11-30 10:50:40 +0000

    test passing

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
+require "./app/models/link.rb"
 
 feature "Homepage" do
   scenario "User expects to see list of links after logging in" do
     Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
 
-    visit './links'
+    visit '/links'
+    expect(page.status_code).to eq 200
 
     within 'ul#links' do
     expect(page).to have_content('Makers Academy')
